a5509686ad3641153888e5a9de389c12c0771f96
some-boi <someboi402@gmail.com>
2022-02-10 17:50:41 +0100

    Update discordeno

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export * from "https://deno.land/x/discordeno@13.0.0-rc18/mod.ts";
-export type { BotWithCache } from "https://deno.land/x/discordeno_cache_plugin@0.0.21/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0-rc19/mod.ts";
+export type { BotWithCache } from "https://deno.land/x/discordeno@13.0.0-rc19/plugins/cache/mod.ts";
 export {
   getMissingChannelPermissions,
   getMissingGuildPermissions,
-} from "https://deno.land/x/discordeno_permissions_plugin@0.0.15/mod.ts";
+} from "https://deno.land/x/discordeno@13.0.0-rc19/plugins/permissions/src/permissions.ts";
