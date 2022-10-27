f0a9eeb05dd6301391069bf21aa2b71e9d4944f1
deepparag <deep.p.sarda@gmail.com>
2022-10-27 15:38:22 +0530

    Fix events

--- src/decorators/EventDecorator.ts
+++ src/decorators/EventDecorator.ts
@@ -1,23 +1,18 @@
 import { cache } from "../cache.ts";
 import { AmethystBot } from "../interfaces/bot.ts";
-import { AmethystEvent, AmethystEvents } from "../interfaces/event.ts";
 
-export function Event(
-  options: AmethystEvent<keyof AmethystEvents>
-): MethodDecorator {
+export function Event(name: string, botCacheNumber?: number): MethodDecorator {
   return function (
     _target: any,
     _propertyKey: string | symbol,
     descriptor: PropertyDescriptor
   ) {
-    options.execute = descriptor.value;
-
     let bot: AmethystBot;
 
     if (cache.size == 1) bot = cache.first()!;
-    else if (options.botCacheNumber) bot = cache.get(options.botCacheNumber)!;
+    else if (botCacheNumber) bot = cache.get(botCacheNumber)!;
     else bot = cache.first()!;
 
-    bot.on(options.name, options.execute);
+    bot.on(name, descriptor.value);
   };
 }
